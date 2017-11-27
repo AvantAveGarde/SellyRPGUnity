a8e527387cdb64027cb9d583acaa0a14945393af
AvantAveGarde <coltonlee4407@hotmail.com>
2017-11-27 03:30:49 -0700

    UI

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+    public Slider healthBar;
+    public Slider chargesBar;
+    public Text HPText;
+    public Text ChargesText;
+    public PlayerUIManager playerUIManger;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        healthBar.maxValue = playerUIManger.playerMaxHP;
+        healthBar.value = playerUIManger.playerHP;
+        chargesBar.maxValue = playerUIManger.playerMaxCharges;
+        chargesBar.value = playerUIManger.playerCharges;
+	}
+}
