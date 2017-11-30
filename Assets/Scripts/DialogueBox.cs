51ca9f5bd7984df7d93ece32c0ae297f71302583
AvantAveGarde <coltonlee4407@hotmail.com>
2017-11-29 22:32:07 -0700

    pretty sloppy intro
    if anyone wants to polish it feel free, it is minor but it's also for flavor stuff I guess.

---
+++ Assets/Scripts/DialogueBox.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DialogueBox : MonoBehaviour {
+    public GameObject dialogueBox;
+    public Text dialogueText;
+
+
+    public bool dialogueActive;
+    public string[] dialogueLines;
+    public int currentLine;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (dialogueActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            currentLine++;
+        }
+
+        if(currentLine >= dialogueLines.Length)
+        {
+            dialogueBox.SetActive(false);
+            dialogueActive = false;
+            currentLine = 0;
+            SceneManager.LoadScene(0);
+        }
+        dialogueText.text = dialogueLines[currentLine];
+    }
+
+    public void ShowBox(string i_dialogue)
+    {
+        dialogueActive = true;
+        dialogueBox.SetActive(true);
+        dialogueText.text = i_dialogue;
+    }
+}
